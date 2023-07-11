ca2f5e1de83a29e48ad5527a065b3fad5c913977
Ericmohn <68788167+Ericmohn@users.noreply.github.com>
2023-07-11 20:09:36 -0300

    Update dice_simulator.py

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,25 +1,35 @@
 # Dice Simulator
 # It rolls a value from 1 and 6.
 import random
+import PySimpleGUI as sg
 
 class DiceSimulator:
     def __init__(self):
         self.min_value = 1
         self.max_value = 6
         self.message = 'Would you like to roll the dice?'
+        #Layout
+        self.layout = [
+            [sg.Text('Would you like to roll the dice?')],
+            [sg.Button('yes'), sg.Button('no')]
+        ]
 
     def Start(self):
-        answer = input(self.message)
+        #Window
+        self.window = sg.Window('Dice Simulator', layout=self.layout)
+        #Read the values
+        self.events, self.values = self.window.Read()
+        #Use the Values
         try:
-            if answer == 'yes' or answer == 'y' : 
-                self.RollDiceValue()
-            elif answer == 'no' or answer == 'n' :
-                print('Goodbye!')
-            else:
-                print('Please type "yes" or "no" ')
+                if self.events == 'yes' or self.events == 'y' : 
+                    self.RollDiceValue()
+                elif self.events == 'no' or self.events == 'n' :
+                    print('Goodbye!')
+                else:
+                    print('Please type "yes" or "no" ')
         except:
             print('There was an error while recieving your answer')
-    
+        
     def RollDiceValue(self):
         print(random.randint(self.min_value, self.max_value))
 
